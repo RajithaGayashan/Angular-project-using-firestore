c5f35ba1ba786e994662a2d329e70a330c57c1cc
RajithaGayashan <rajithagayashan95@gmail.com>
2020-03-14 14:56:17 +0530

    form validation added

--- src/app/all-admin/admin/admin.component.ts
+++ src/app/all-admin/admin/admin.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Adminservice } from 'src/app/shared/admin.service';
+import {FormControl, FormGroup, Validators, NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
-  templateUrl: './admin.component.html',
+    templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  constructor(private _service : Adminservice) { }
 
-  constructor(private service : Adminservice) { }
-
+  error: string;
   ngOnInit() {
   }
 
+  resetForm(form ?:NgForm){
+    if (form!=null)
+    form.resetForm();
+    this._service.formData={
+      firstName : '' ,
+      lastName : '' ,
+      position : '' ,
+      adminCode : null ,
+      mobile: '' ,
+      gender: '' ,
+      email: '' ,
+      password : ''
+
+    }
+  }
 }
